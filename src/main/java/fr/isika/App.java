890dc4d02a89d48e5e4ef482a3201c1629cd23a3
adriencr81 <adrien.deleuil@hotmail.fr>
2022-06-09 17:04:55 +0200

    commit à partir d'eclipse

---
+++ src/main/java/fr/isika/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.isika;
+
+/**
+ * @author operateur
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
